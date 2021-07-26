8174cd8127d8d18997312459dbeb9731fb4a0214
matia005 <matia0529@gmail.com>
2021-07-26 12:43:24 +0900

    기본 변수의 이해

---
+++ src/day02/Ex01.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		/*
+		 변수 작성 방법
+		 자료형 변수 이름
+		 자료형 변수 이름 = 값;
+		 변수 
+		 */
+		
+		
+	}
+}
---
+++ src/day02/Ex02.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		int age = 22;
+		double weight = 108.3;
+		double height = 183.2;
+		System.out.println("나의 나이는"+age +"세 입니다.");
+		System.out.println("나의 몸무게는 "+weight+ "(kg) 입니다.");
+		System.out.println("나의 키는 " +height+"(cm) 입니다.");
+		
+	}
+}
